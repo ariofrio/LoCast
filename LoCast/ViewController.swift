5c70879fdaaaab392123e2899796d51ef3b90f20
Andres Riofrio <riofrios@gmail.com>
2018-05-12 18:35:46 -0700

    Initial Commit

---
+++ LoCast/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoCast
+//
+//  Created by user140378 on 5/12/18.
+//  Copyright © 2018 Andres Riofrio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
